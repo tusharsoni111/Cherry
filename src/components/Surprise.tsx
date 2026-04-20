077f98fc18ddde66337ecb2234f6783733ed379b
tusharsoni111 <110826639+tusharsoni111@users.noreply.github.com>
2026-04-20 12:33:24 +0530

    Update Surprise.tsx

--- src/components/Surprise.tsx
+++ src/components/Surprise.tsx
@@ -16,8 +16,8 @@ const Surprise = () => {
     "Play Music",
     "Decorate",
     "Fly the Balloons",
-    "Let's Cut the Cake Madam Ji",
-    "Well, I Have a Message for You Madam Ji"
+    "Tap to see The girl Behind Sahiba theme",
+    "Well, I Have a Message for You"
   ];
 
   const handleClick = () => {
