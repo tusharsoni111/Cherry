13bd07399806add9440e801c5b5fdf134c628912
tusharsoni111 <110826639+tusharsoni111@users.noreply.github.com>
2026-04-20 23:35:15 +0530

    Update Message.tsx

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -11,16 +11,20 @@ const Message = () => {
         className="max-w-2xl bg-white/80 backdrop-blur-sm p-8 rounded-xl shadow-xl text-center"
       >
         <h1 className="text-3xl font-bold text-gray-800 mb-6">
-          Dear Madam Ji,
+          Dear Cherry,
         </h1>
         <p className="text-xl text-gray-700 leading-relaxed mb-6">
-          Even though we have met recently & i guess we know nothing about eachother but this
-          is my kindness to show you how much i value people.This is a way 
-          to show that our friendship just started and i hope we will be making some great memories together.
-          I'm starting this new year with a new friend as you and i hope our friendship will last forever.
+         Tu thodi confusing hai kabhi full strong “mujhe kisi ki zarurat nahi”,
+          aur kabhi bas thoda sa real connection chahiye hota hai. 
+          But honestly? That just makes you real.
+           Mujhe pata hai kabhi kabhi tera mind hi tere against chala jata hai… tu khud pe doubt karti hai.
+          But still tu har din fight krleti h , Aur woh chhoti baat nahi hai while facing the anti feminist enviroment around you.
+          Keep carring with flow " maa-chudaye mard jaat wala attitude"
         </p>
         <p className="text-lg text-pink-600 font-semibold">
-          and on your special day, i wish you the happiest birthday
+         What makes u special to me is,
+         you can sense when i am sad even on the days i dont speak up.
+        So yeah… happy birthday. Sach mein, I’m really glad tu hai.
         </p>
       </motion.div>
     </div>
